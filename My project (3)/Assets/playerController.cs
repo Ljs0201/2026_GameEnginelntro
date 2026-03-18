fd73a579ce1e1761fa03a6e20f519ec944622e40
Ljs0201 <a01029950314@gmail.com>
2026-03-18 16:05:19 +0900

    Add : 플레이어 애니메이션, 점프추가

--- My project (3)/Assets/playerController.cs
+++ My project (3)/Assets/playerController.cs
@@ -4,14 +4,26 @@ public class playerController : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myanimator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        myanimator = GetComponent<Animator>();
+        myanimator.SetBool("move", false);
     }
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
     void Update()
     {
@@ -24,7 +36,16 @@ public class playerController : MonoBehaviour
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+
+        if(moveInput.magnitude > 0)
+        {
+            myanimator.SetBool("move", true);
+        }
+        else
+        {
+            myanimator.SetBool("move", false);
+        }
+            transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
 
     }
 }
